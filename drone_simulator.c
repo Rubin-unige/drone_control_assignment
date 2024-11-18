a635a94555ec542ef206ef0c92fc313503a5c48e
Rubin Khadka <rubin.robotic@gmail.com>
2024-11-18 19:35:51 +0100

    basic ncurses with box

--- drone_simulator.c
+++ drone_simulator.c
@@ -4,22 +4,24 @@
 
 int main() {
 
+    initscr();            // Start ncurses mode
+
     // Define window dimensions
     int height = 20, width = 50, start_y = 0, start_x = 0;
     WINDOW *win = newwin(height, width, start_y, start_x);
 
-    // Main game loop
+    // Main simulatio board
     int ch;
     mvwprintw(win, 1, 1, "Drone Simulator");
-    box(win, 0, 0); // Draw border
+    box(win, 0, 0);
     wrefresh(win);
 
-    while ((ch = getch()) != 'q') { // Press 'q' to quit
+    while ((ch = getch()) != 'q') { 
         werase(win);
         mvwprintw(win, 1, 1, "Key pressed: %c", ch);
         box(win, 0, 0);
         wrefresh(win);
-        usleep(100000); // Sleep for 100ms
+        usleep(100000); 
     }
 
     delwin(win);
